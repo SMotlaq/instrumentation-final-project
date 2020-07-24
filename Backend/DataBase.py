68a1326774f0292bdecc289b9b24e6621085d73b
S_Motlaq <motlaq@aut.ac.ir>
2020-07-25 01:42:02 +0430

    template updated

--- Backend/DataBase.py
+++ Backend/DataBase.py
@@ -1,9 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-import numpy.matlib as npm
-import os
-
-db_address = os.path.dirname(os.path.abspath(__file__)) + '\database.db'
 
 def create_connection(db_file):
     conn = None
@@ -12,304 +8,69 @@ def create_connection(db_file):
     except Error as e:
         print(e)
     return conn
-
-
-def delete_all_rows_from_link_table(conn,src,dest):
-    if conn is not None:
-        sql = " DELETE FROM {}_to_{} ".format(src,dest)
-        try:
-            c = conn.cursor()
-            c.execute(sql)
-        except Error as e:
-            print(e)
-
-
 def create_table(conn):
     if conn is not None:
-        sql = """ CREATE TABLE IF NOT EXISTS nodes (
+        sql = """ CREATE TABLE IF NOT EXISTS users (
                     id integer PRIMARY KEY AUTOINCREMENT,
+                    uwer_name text,
+                    password_hashed text,
                     name text,
-                    type text,
-                    position_x float,
-                    position_y float
+                    parking_number text,
+                    credit text
                   ); """
         try:
             c = conn.cursor()
             c.execute(sql)
         except Error as e:
             print(e)
-    
-def add_nodes(conn, node):
+
+def add_user(conn, user_name, password_hashed, name, parking_number=None, credit='0'):
     try:
-        sql = ''' INSERT INTO nodes(name, type, position_x, position_y)
-                  VALUES(?,?,?,?) '''
+        sql = ''' INSERT INTO users(user_name, password_hashed, name, parking_number, credit)
+                  VALUES(?,?,?,?,?) '''
         cur = conn.cursor()
-        cur.execute(sql, node)
-        print('hy')
+        cur.execute(sql, (user_name, password_hashed, name, parking_number, credit,))
         return cur.lastrowid
     except Error as e:
         print(e)
-
-def add_links(conn, link):
-    sql = ''' INSERT INTO links(name, type, alpha_index, gamma_index, beta1_index, beta2_index, beta3_index, D_index, gain, distance, source, destination)
-              VALUES(?,?,?,?,?,?,?,?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, link)
-    return cur.lastrowid
-
-def delete_node(conn, id):
-    #------------------------------delete connected links------------------------------
+def query_user(conn, user_name):
     try:
         cur = conn.cursor()
-        cur.execute('DELETE FROM links WHERE source=? OR destination=?', (id,id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-    
-    #-------------------------correct source & destination IDs-------------------------
-    try:
-        cur = conn.cursor()
-        cur.execute('UPDATE links SET source=source-1 WHERE source>?', (id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-
-    try:
-        cur = conn.cursor()
-        cur.execute('UPDATE links SET destination=destination-1 WHERE destination>?', (id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-    #------------------------------------delete node-----------------------------------
-    try:
-        cur = conn.cursor()
-        cur.execute('DELETE FROM nodes WHERE id=?', (id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-    #----------------------------------resort node IDs---------------------------------    
-    try:
-        cur = conn.cursor()
-        cur.execute('UPDATE nodes SET id=id-1 WHERE id>?', (id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-
-def delete_link(conn, id):
-    try:
-        cur = conn.cursor()
-        cur.execute('DELETE FROM links WHERE id=?', (id,))
-        conn.commit()
-    except Error as e:
-        print(e)
-
-def max_node_id(conn):
-    try:    
-        cur = conn.cursor()
-        cur.execute('SELECT MAX(id) FROM nodes')
-        row = cur.fetchall()
-        return row[0][0]
-    except Error as e:
-        print(e)
-        return 0
-
-def query_link_id(conn, source, destination):
-    try:
-        cur = conn.cursor()
-        cur.execute('SELECT id FROM links WHERE source=? AND destination=?', (source, destination,))
+        cur.execute('SELECT * FROM users WHERE user_name=?', (user_name,))
         link_id = cur.fetchall()
         if link_id==[]:
             return 0
         else:
-            return link_id[0][0]
+            return link_id[0]
     except Error as e:
         print(e)
         return 0
-
-def make_matrix(conn):
-    matrix_size = max_node_id(conn)
-    out = npm.zeros((matrix_size,matrix_size))
-    for row in range(matrix_size):
-        for column in range(matrix_size):
-            out[row,column] = query_link_id(conn, column+1, row+1)
-    return out+out.transpose()
-
-def update_node_properties(conn, id, newName=None, newType=None, newX=None, newY=None):
-    if newName is not None:
+def update_user(conn, user_name, password_hashed=None, name=None, parking_number=None, credit=None):
+    if password_hashed is not None:
         try:
             cur = conn.cursor()
-            cur.execute('UPDATE nodes SET name=? WHERE id=?', (newName,id,))
+            cur.execute('UPDATE users SET password_hashed=? WHERE user_name=?', (password_hashed, user_name,))
             conn.commit()
         except Error as e:
             print(e)
-    if newType is not None:
+    if name is not None:
         try:
             cur = conn.cursor()
-            cur.execute('UPDATE nodes SET type=? WHERE id=?', (newType,id,))
+            cur.execute('UPDATE users SET name=? WHERE user_name=?', (name, user_name,))
             conn.commit()
         except Error as e:
             print(e)
-    if newX is not None:
+    if parking_number is not None:
         try:
             cur = conn.cursor()
-            cur.execute('UPDATE nodes SET position_x=? WHERE id=?', (newX,id,))
+            cur.execute('UPDATE users SET parking_number=? WHERE user_name=?', (parking_number, user_name,))
             conn.commit()
         except Error as e:
             print(e)
-    if newY is not None:
+    if credit is not None:
         try:
             cur = conn.cursor()
-            cur.execute('UPDATE nodes SET position_y=? WHERE id=?', (newY,id,))
+            cur.execute('UPDATE users SET credit=? WHERE user_name=?', (credit, user_name,))
             conn.commit()
         except Error as e:
             print(e)
-
-def update_link_properties(conn, id, newName=None, newType=None, newAlphaIndex=None, 
-                           newGammaIndex=None, newBetaIndex1=None, newBetaIndex2=None, 
-                           newBetaIndex3=None, newDIndex=None, newGain= None, 
-                           newDistance=None, newSource=None, newDestination=None):
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET name=? WHERE id=?', (newName,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET type=? WHERE id=?', (newType,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET alpha_index=? WHERE id=?', (newAlphaIndex,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET gamma_index=? WHERE id=?', (newGammaIndex,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET beta1_index=? WHERE id=?', (newBetaIndex1,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET beta2_index=? WHERE id=?', (newBetaIndex2,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET beta3_index=? WHERE id=?', (newBetaIndex3,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET D_index=? WHERE id=?', (newDIndex,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET gain=? WHERE id=?', (newGain,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET distance=? WHERE id=?', (newDistance,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET source=? WHERE id=?', (newSource,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-    if newName is not None:
-        try:
-            cur = conn.cursor()
-            cur.execute('UPDATE links SET destination=? WHERE id=?', (newDestination,id,))
-            conn.commit()
-        except Error as e:
-            print(e)
-
-
-def create_results_table(conn, node_num):
-    if conn is not None:
-        sql = "DROP TABLE IF EXISTS results;"
-        try:
-            c = conn.cursor()
-            c.execute(sql)
-        except Error as e:
-            print(e)
-        sql = """ CREATE TABLE IF NOT EXISTS results (
-                    id integer,
-                    source text,
-                    destination text,
-                    type text,
-                    wavelengthIndex integer,
-                    OSNR float,
-                    length float, 
-                    protOSNR float, 
-                    protLength float
-                  ); """
-        try:
-            c = conn.cursor()
-            c.execute(sql)
-        except Error as e:
-            print(e)
-
-        for num in range(node_num):  # main path nodes
-            num = num + 1
-            sql = "ALTER TABLE results ADD COLUMN '{}' text".format(str(num))
-            try:
-                c = conn.cursor()
-                c.execute(sql)
-            except Error as e:
-                print(e)
-        for num in range(node_num):  # protection path nodes
-            num = num + 1
-            sql = "ALTER TABLE results ADD COLUMN '{}' text".format(str(num+100))
-            try:
-                c = conn.cursor()
-                c.execute(sql)
-            except Error as e:
-                print(e)
-        for num in range(node_num):  # main path regenerators
-            num = num + 1
-            sql = "ALTER TABLE results ADD COLUMN '{}' text".format(str(num+200))
-            try:
-                c = conn.cursor()
-                c.execute(sql)
-            except Error as e:
-                print(e)
-        for num in range(node_num):  # protection path regenerators
-            num = num + 1
-            sql = "ALTER TABLE results ADD COLUMN '{}' text".format(str(num+300))
-            try:
-                c = conn.cursor()
-                c.execute(sql)
-            except Error as e:
-                print(e)
-    else:
-        print('Error: cannot connect to database')
--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import DataBase as db
-import tlg
 import datetime
 import requests
 import passwords
@@ -11,185 +10,41 @@ conn         = db.create_connection(database)
 mainPassword = password.mainPassword
 db.create_table(conn)
 
-#------------------------------ESP requests------------------------------#
-@app.route('/fac/status', methods = ['GET'])
-def whatsup():
+#------------------------------ ESP requests ------------------------------#
+@app.route('/get_status', methods = ['GET'])
+def get_status():
     password = request.args.get('password')
     if password==mainPassword:
-        global global_state
-        foo  = global_state
-        global_state = '0'
-        return foo
+        return None
     else:
         return 'wrong password'
 
-@app.route('/fac/authen', methods = ['POST'])
-def authenticate():
+@app.route('/set_status', methods = ['POST'])
+def set_status():
     password = request.args.get('password')
     if password==mainPassword:
-        global conn
-        response = '0'
-        currentDT = datetime.datetime.now()
-        day_corrected = ['3','4','5','6','7','1','2']
-        cardID = request.args.get('cardID')
-        PIR    = request.args.get('PIR')
-        id = db.query(conn, 'SELECT id FROM members WHERE cardID=?', (cardID,))
-        if id==[]:
-            response = '0 - id reject'
-        else:
-            groups = db.query(conn, 'SELECT group_id FROM permissions WHERE std_id=?', (id[0][0],))
-            if groups==[]:
-                return '404 - no permission defined for this user :('
-            for group in groups:
-                times = db.query(conn, 'SELECT from_,to_,day FROM groups WHERE id=?', (group[0][0],))
-                from_, to_, day_ = datetime.datetime.strptime(times[0][0],"%H:%M:%S").time(), datetime.datetime.strptime(times[0][1],"%H:%M:%S").time(), times[0][2]
-                if day_corrected[currentDT.weekday()] not in day_:
-                    response = '0 - day reject'
-                elif from_<currentDT.time() and to_>currentDT.time():
-                    response = '1'
-                    log(conn, cardID, response, PIR)
-                    return '1'
-                else:
-                    response = '0 - time reject'
-        log(conn, cardID, response, PIR)
-        return response
+        return None
     else:
         return 'wrong password'
 
-@app.route('/fac/addUser', methods = ['POST'])
+#----------------------------- Admin requests -----------------------------#
+@app.route('/addUser', methods = ['POST'])
+#user_name, password_hashed, name
 def addUser():
-    global conn
-    cardID   = request.args.get('cardID')
-    stdNum   = request.args.get('stdNum')
     password = request.args.get('password')
     if password==mainPassword:
-        with conn:
-            db.update_cardID(conn, stdNum, cardID,)
-        return 'added'
+        return None
     else:
         return 'wrong password'
 
-#-----------------------------Admin requests-----------------------------#
-@app.route('/fac/new', methods = ['POST'])
-def newMember():
+@app.route('/parking_action', methods = ['POST'])
+# password, address, action
+def parking_action():
     password = request.args.get('password')
     if password==mainPassword:
-        global conn
-        global global_state
-        stdNum = request.args.get('stdNum')
-        name   = request.args.get('name')
-        family = request.args.get('family')
-        groups = request.args.get('groups')
-        isGirl = request.args.get('isGirl')
-        with conn:
-            id_ = db.add_member(conn, (stdNum, 'Null', name, family, isGirl,))
-            for group in groups:
-                db.add_permission(conn, (id_, group,))
-        global_state = str(stdNum)
-        return 'wating for cardID'
+        return None
     else:
         return 'wrong password'
-
-@app.route('/fac/config/groups', methods = ['POST'])
-def groups():
-    password = request.args.get('password')
-    if password==mainPassword:
-        global conn
-        raw_data = request.get_json()
-        if raw_data['add']!={}:
-            with conn:
-                db.add_group(conn, (raw_data['add']['name'], raw_data['add']['from'],
-                                    raw_data['add']['to'], raw_data['add']['days'],))
-        if raw_data['remove']!={}:
-            with conn:
-                db.delete_group(conn, raw_data['remove'])
-        if raw_data['edit']!={}:
-            with conn:
-                if raw_data['edit']['name']!='Null':
-                    db.update_groups(conn, 'UPDATE groups SET groupName=? WHERE id=?', (raw_data['edit']['name'],raw_data['edit']['id'],))
-                if raw_data['edit']['from']!='Null':
-                    db.update_groups(conn, 'UPDATE groups SET from_=? WHERE id=?', (raw_data['edit']['from'],raw_data['edit']['id'],))
-                if raw_data['edit']['to']!='Null':
-                    db.update_groups(conn, 'UPDATE groups SET to_=? WHERE id=?', (raw_data['edit']['to'],raw_data['edit']['id'],))
-                if raw_data['edit']['days']!='Null':
-                    db.update_groups(conn, 'UPDATE groups SET day=? WHERE id=?', (raw_data['edit']['days'],raw_data['edit']['id'],))
-        return 'Done'
-    else:
-        return 'wrong password'
-
-@app.route('/fac/config/members', methods = ['POST'])
-def members():
-    password = request.args.get('password')
-    if password==mainPassword:
-        global conn
-        raw_data = request.get_json()
-        if raw_data['remove']!={}:
-            with conn:
-                db.delete_member(conn, stdNum = raw_data['remove'])
-        return 'Done'
-    else:
-        return 'wrong password'
-
-@app.route('/fac/config/permissions', methods = ['POST'])
-def permissions():
-    password = request.args.get('password')
-    if password==mainPassword:
-        global conn
-        raw_data = request.get_json()
-        if raw_data['add']!={}:
-            with conn:
-                db.add_permission(conn, raw_data['add']['stdNum'], raw_data['add']['groupID'])
-        if raw_data['remove']!={}:
-            with conn:
-                db.delete_permission(conn, raw_data['remove']['stdNum'], raw_data['remove']['groupID'])
-        return 'Done'
-    else:
-        return 'wrong password'
-
-@app.route('/fac/config/open', methods = ['POST'])
-def open():
-    password = request.args.get('password')
-    if password==mainPassword:
-        global global_state
-        global_state = '1'
-        return 'command sent'
-    else:
-        return 'wrong password'
-
-
-def log(conn, cardID, response, session):
-    currentDT = datetime.datetime.now().weekday()
-    day_corrected = ['3','4','5','6','7','1','2']
-    time = requests.get("http://gahshomar-api.herokuapp.com/zone/Asia-Tehran").text.replace(' ','').split(',')[0]
-    date = requests.get("http://gahshomar-api.herokuapp.com/date/jalali"     ).text.replace(' ','')
-    time, date = cleaner(time, date)
-    if response == '0 - id reject':
-        tlg.send('⚠️ Unsuccessful attempt (unknown card) ⚠️\n💳 Unique ID: ' + cardID + '\n🕗  ' + time + '\n🗓  ' + date)
-        with conn:
-            db.add_log(conn, ('Null',cardID, 'Null', 'Null', date, time, session, 'failed',))
-        return 0
-    if response == '1':
-        status = 'granted'
-    else:
-        status = 'failed'
-    member = db.query(conn,'SELECT stdNum, name, family, isGirl FROM members WHERE cardID=?', (cardID,))
-    with conn:
-        db.add_log(conn, (member[0][0], cardID, member[0][1], member[0][2], date, time, session, status,))
-    name   = member[0][1]
-    family = member[0][2]
-    gender_icon  = {1:'🙍🏻‍♀️‍ ',
-                    0:'🙍🏻‍♂️ '}
-    status_text  = {'granted':'✅ Granted  \n',
-                    'failed' :'❌ Rejected \n'}
-    weekday_name = {'1':'شنبه',
-                    '2':'یکشنبه',
-                    '3':'دوشنبه',
-                    '4':'سه شنبه',
-                    '5':'چهارشنبه',
-                    '6':'پنجشنبه',
-                    '7':'جمعه'}
-    tlg.send(status_text[status] + gender_icon[member[0][3]] + name + ' ' + family + '\n🕗  ' + time + '\n🗓  ' + date + '\n  ' + weekday_name[day_corrected[currentDT]])
-    return 0
 
 def cleaner(time, date):
     time_elements = time.split(':')
@@ -209,6 +64,11 @@ def cleaner(time, date):
     newTime = time_elements[0]+':'+time_elements[1]+':'+time_elements[2]
     newDate = date_elements[2]+'/'+date_elements[1]+'/'+date_elements[0]
     return newTime, newDate
+def getTime():
+    time = requests.get("http://gahshomar-api.herokuapp.com/zone/Asia-Tehran").text.replace(' ','').split(',')[0]
+    date = requests.get("http://gahshomar-api.herokuapp.com/date/jalali"     ).text.replace(' ','')
+    time, date = cleaner(time, date)
+    return time, date
 
 if __name__ == '__main__':
-    app.run(host='178.63.211.120')
+    app.run()#host='178.63.211.120')
