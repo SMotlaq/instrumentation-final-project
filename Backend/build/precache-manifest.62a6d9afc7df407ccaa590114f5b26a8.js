e2e17c9c92ace06a8fba07e0e1269c3407364564
S_Motlaq <motlaq@aut.ac.ir>
2020-08-05 19:39:47 +0430

    added react files

---
+++ Backend/build/precache-manifest.62a6d9afc7df407ccaa590114f5b26a8.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b1039f6bc161c738203a08c5e4bce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54a383a55b5d8e22eda4",
+    "url": "/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "977cf88ae3315197e76b",
+    "url": "/static/css/main.6bf018b9.chunk.css"
+  },
+  {
+    "revision": "54a383a55b5d8e22eda4",
+    "url": "/static/js/2.226b4efe.chunk.js"
+  },
+  {
+    "revision": "e59abd090c9b9dfc2638912f8e93e3b7",
+    "url": "/static/js/2.226b4efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977cf88ae3315197e76b",
+    "url": "/static/js/main.575a20fb.chunk.js"
+  },
+  {
+    "revision": "944cfaf9598868733f86",
+    "url": "/static/js/runtime-main.6264c053.js"
+  },
+  {
+    "revision": "ebe074e1adc744cb898dc7cca279fb42",
+    "url": "/static/media/Cover Photo.ebe074e1.jpg"
+  },
+  {
+    "revision": "0bc3200ee642841a84ef4b6f33685ccd",
+    "url": "/static/media/Flaticon.0bc3200e.woff"
+  },
+  {
+    "revision": "491bf3d9d4a49a9b418f8635a913d35a",
+    "url": "/static/media/Flaticon.491bf3d9.ttf"
+  },
+  {
+    "revision": "4dbc0d688802b84fc77b306cad405311",
+    "url": "/static/media/Flaticon.4dbc0d68.eot"
+  },
+  {
+    "revision": "5c19cf8bd1c8b9e7ccf30130a7d7cf55",
+    "url": "/static/media/Flaticon.5c19cf8b.woff2"
+  },
+  {
+    "revision": "7df4aca5d33a467557bcbb507d760ee4",
+    "url": "/static/media/Flaticon.7df4aca5.svg"
+  },
+  {
+    "revision": "27fe38dd94556dddf1a7c7a4574e460f",
+    "url": "/static/media/Vazir-Black.27fe38dd.ttf"
+  },
+  {
+    "revision": "806c5f888bbee3732fd1226ab969c427",
+    "url": "/static/media/Vazir-Black.806c5f88.woff2"
+  },
+  {
+    "revision": "8f32bc13f6d38153304ade14dbd9c41d",
+    "url": "/static/media/Vazir-Black.8f32bc13.woff"
+  },
+  {
+    "revision": "c3508a6d524a2c43a977fe4517eeb72a",
+    "url": "/static/media/Vazir-Black.c3508a6d.eot"
+  },
+  {
+    "revision": "3c6793f7e4cb57268b873b6cb1b524da",
+    "url": "/static/media/Vazir-Bold.3c6793f7.ttf"
+  },
+  {
+    "revision": "49b2fd954333c7776d24b495499605c0",
+    "url": "/static/media/Vazir-Bold.49b2fd95.woff2"
+  },
+  {
+    "revision": "76d07739283e591ff556fd98eb56446c",
+    "url": "/static/media/Vazir-Bold.76d07739.eot"
+  },
+  {
+    "revision": "c2fa1879fb42bddde529e48b5adcbf7c",
+    "url": "/static/media/Vazir-Bold.c2fa1879.woff"
+  },
+  {
+    "revision": "14235baef5da52f9dcba69534d4248d0",
+    "url": "/static/media/Vazir-Light.14235bae.woff2"
+  },
+  {
+    "revision": "a84d1efca8cb2768b80b56d4551c2825",
+    "url": "/static/media/Vazir-Light.a84d1efc.ttf"
+  },
+  {
+    "revision": "b43f03fc0b4d9afc5a1100a69988acc3",
+    "url": "/static/media/Vazir-Light.b43f03fc.eot"
+  },
+  {
+    "revision": "e8b47541c211a778f3805405ec3a61e7",
+    "url": "/static/media/Vazir-Light.e8b47541.woff"
+  },
+  {
+    "revision": "1732c9111ba1564b86662746a1bc4592",
+    "url": "/static/media/Vazir-Medium.1732c911.woff"
+  },
+  {
+    "revision": "adb54a43a83fd3bf92eef575ab4b6dd4",
+    "url": "/static/media/Vazir-Medium.adb54a43.ttf"
+  },
+  {
+    "revision": "d1db0d34c838a73f49103d01350c627a",
+    "url": "/static/media/Vazir-Medium.d1db0d34.eot"
+  },
+  {
+    "revision": "f5c733af39b3d6919d1cdb421c2dff0e",
+    "url": "/static/media/Vazir-Medium.f5c733af.woff2"
+  },
+  {
+    "revision": "231179b1dce458a63abe017a8250d9ff",
+    "url": "/static/media/Vazir-Thin.231179b1.woff2"
+  },
+  {
+    "revision": "2e741f61fad41c202f2fe80f422a0479",
+    "url": "/static/media/Vazir-Thin.2e741f61.woff"
+  },
+  {
+    "revision": "4c81b3370c77f56a93802369546156a0",
+    "url": "/static/media/Vazir-Thin.4c81b337.eot"
+  },
+  {
+    "revision": "c2eb951dd1b9354dc08e76d4d87d4da4",
+    "url": "/static/media/Vazir-Thin.c2eb951d.ttf"
+  },
+  {
+    "revision": "776679787764c87966edb49c4b04457f",
+    "url": "/static/media/Vazir.77667978.woff"
+  },
+  {
+    "revision": "7fd2ea03496a39cf6944b7dc01100f48",
+    "url": "/static/media/Vazir.7fd2ea03.eot"
+  },
+  {
+    "revision": "e2f47dc27322febdab9b8d0293b405f5",
+    "url": "/static/media/Vazir.e2f47dc2.ttf"
+  },
+  {
+    "revision": "ffad2a1d7bd76954a41dd32d3337ce0e",
+    "url": "/static/media/Vazir.ffad2a1d.woff2"
+  }
+]);
